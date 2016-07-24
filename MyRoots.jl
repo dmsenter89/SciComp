2229aff78fab7e07e4fc89f8ec4fbbca26c1d4a0
Michael Senter <Michael Senter>
2016-07-24 12:27:31 -0400

    First Commit
    Wrote entry for readme and added an (incomplete) Julia module for finding
    roots of equations.

---
+++ MyRoots.jl
@@ -0,0 +1,58 @@
+module MyRoots
+#=
+	Julia module to find roots of equations using some standard methods.
+=#
+
+function bisect(f,a,b; TOL=1e-9, MAXN=3000, del=1e-9)
+    # computes f(x)=0 using bisection algorithm
+    u = f(a); 
+    v = f(b);
+    e = b-a;
+    if sign(u)==sign(v)
+        throw(DomainError());
+    end
+    for k=1:MAXN
+        e = e/2;
+        c = a+e;
+        w = f(c);
+        if (abs(e)<del) || (abs(w)<TOL)
+            return c;
+        end
+        if sign(w)!=sign(u)
+            b = c;
+            v = w;
+        else
+            a = c;
+            u = w;
+        end
+    end
+end
+
+function newton(p0,f,df; TOL=1e-9, MAXN=3000)
+    # Function to calculate root f(x)=0 using Newton's method.
+    # Requires initial guess p0 and functions f, df.
+    # Optional tolerance and max-iter settings.
+    # Returns root if found, throws bound error otherwise.
+  for i=1:MAXN
+    while i<=MAXN
+      p=p0 - f(p0)/df(p0);
+      if abs(p-p0)<TOL
+        return p
+      end
+      i+=1;
+      p0=p;
+    end
+  end
+  throw(BoundsError);
+end
+
+function secant(p0,f;TOL=1e-5, MAXN=2000)
+    #= Func
+    =#
+    for i=1:MAXN
+        # actually fill this inn
+        println("Hello.")
+    end
+end
+
+end
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# SciComp
+# SciComp Repo
+This repo is a collection of files I am collecting through grad school for solving numerical analysis problems using standard algorithms.
